db2dce6d534f0068c3f0acb214be55fadaac3346
Apple <apple@MacBook-Pro.local>
2021-06-18 21:29:15 +0700

    Create project

---
+++ DuQuynhIOS/DuQuynhIOS/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  DuQuynhIOS
+//
+//  Created by Apple on 18/06/2021.
+//
+
+import UIKit
+
+class SecondViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DuQuynhIOS/DuQuynhIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DuQuynhIOS
+//
+//  Created by Apple on 18/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
