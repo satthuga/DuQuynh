fad8610d6787d42a714146b608ba19a10997648f
Chu Du <dunt20082000@gmail.com>
2021-06-18 21:50:20 +0700

    tao man 2

--- DuQuynhIOS/DuQuynhIOS/SecondViewController.swift
+++ DuQuynhIOS/DuQuynhIOS/SecondViewController.swift
@@ -9,10 +9,11 @@ import UIKit
 
 class SecondViewController: ViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        myLabel.text = "Man 2"
     }
 
 
